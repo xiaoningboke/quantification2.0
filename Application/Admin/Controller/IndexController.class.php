1ff15572a2beb81285460695e1f831bfc08138b6
xiaoningboke <sunxiaoning@aliyun.comm>
2018-09-05 21:06:24 +0800

    ...

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+	//显示首页
+    public function index(){
+        $this->display();
+    }
+    //显示学生信息
+    public function student(){
+    	$this->display();
+    }
+    //查询所有学生接口
+    public function selStudent(){
+    	$stu = M('Student');
+    	$count = (int)$stu->count();
+    	$date = $stuMen = $stu->select();
+    	/*var_dump($count);
+    	echo(json_encode($date));*/
+    	$this->returnMsg('1','',$count,$date);
+    }
+    protected function returnMsg($code, $errmsg, $count, $date)
+    {
+        $data['code'] = 0 ;
+        $data['msg'] = $errmsg ? $errmsg : '';
+        $data['count'] = $count ? $count : '';
+        $data['date'] = $date ? $date : '';
+        $this->ajaxReturn($data);
+    }
+}
